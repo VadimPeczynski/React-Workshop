1aab2c8f84e7e1195db0fd889182ffdf0bda4333
Vadim Peczyński <vadim.peczynski@gmail.com>
2021-09-03 11:32:22 +0200

    Homework for Workshop 1

---
+++ 1/homework/heroes-academy/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HeroesList from "./HeroesList";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Witajcie w Heroes Academy!</h1>
+      <HeroesList />
+    </div>
+  );
+}
+
+export default App;
