fa53ab66247ead572a6e15051b356061317bda01
Vadim Peczyński <vadim.peczynski@gmail.com>
2021-09-28 10:56:06 +0200

    Start project for Workshop 3

---
+++ 3/start/heroes-academy/src/components/HeroesList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import HeroesTable from "./HeroesTable";
+
+class HeroesList extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <h5 className="card-header">Lista Herosów</h5>
+        <div className="card-body">
+          <div className="table-responsive"></div>
+          <HeroesTable />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HeroesList;
---
+++ 3/start/heroes-academy/src/components/HeroesTable.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { getHeroes } from "../api/heroesApi";
+import Fist from "./Fist";
+import "../styles/table.css";
+import { Link } from "react-router-dom";
+
+function HeroesTable() {
+  const [heroes, setHeroes] = useState([]);
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    getHeroes().then((_heroes) => {
+      setHeroes(_heroes);
+    });
+  }, []);
+
+  function onLogoButtonClick() {
+    setShowImage(!showImage);
+  }
+
+  if (!(heroes && heroes.length)) {
+    return null;
+  }
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>
+            <button
+              className={showImage ? "btn btn-danger" : "btn btn-success"}
+              type="button"
+              onClick={onLogoButtonClick}
+            >
+              {showImage ? "Ukryj" : "Pokaż"} logo
+            </button>
+          </th>
+          <th>Nazwa</th>
+          <th>Team</th>
+          <th>Tożsamość</th>
+          <th>Wynagrodzenie</th>
+          <th>Siła</th>
+        </tr>
+      </thead>
+      <tbody>
+        {heroes.map((hero) => {
+          return (
+            <tr key={hero.id}>
+              <td>
+                {showImage && (
+                  <img className="image" src={hero.logoUrl} alt={hero.name} />
+                )}
+              </td>
+              <td>
+                <Link to={"/hero-details/" + hero.id}>{hero.name}</Link>
+              </td>
+              <td>{hero.team}</td>
+              <td>{hero.secretIdentity}</td>
+              <td>{hero.salary} PLN</td>
+              <td>
+                <Fist strength={hero.strength} />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+export default HeroesTable;
