00e1e44109f5949eeccb2f26c5de893ac761ad88
Vadim Peczyński <vadim.peczynski@gmail.com>
2021-10-06 11:13:25 +0200

    Improvements for start project for Workshop 2

--- 2/start/heroes-academy/src/components/HeroesTable.js
+++ 2/start/heroes-academy/src/components/HeroesTable.js
@@ -21,44 +21,46 @@ function HeroesTable() {
   }
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>
-            <button
-              className={showImage ? "btn btn-danger" : "btn btn-success"}
-              type="button"
-              onClick={onLogoButtonClick}
-            >
-              {showImage ? "Ukryj" : "Pokaż"} logo
-            </button>
-          </th>
-          <th>Nazwa</th>
-          <th>Team</th>
-          <th>Tożsamość</th>
-          <th>Wynagrodzenie</th>
-          <th>Siła</th>
-        </tr>
-      </thead>
-      <tbody>
-        {heroes.map((hero) => {
-          return (
-            <tr key={hero.id}>
-              <td>
-                {showImage && (
-                  <img className="image" src={hero.logoUrl} alt={hero.name} />
-                )}
-              </td>
-              <td>{hero.name}</td>
-              <td>{hero.team}</td>
-              <td>{hero.secretIdentity}</td>
-              <td>{hero.salary} PLN</td>
-              <td>{hero.strength}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="table-responsive">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              <button
+                className={showImage ? "btn btn-danger" : "btn btn-success"}
+                type="button"
+                onClick={onLogoButtonClick}
+              >
+                {showImage ? "Ukryj" : "Pokaż"} logo
+              </button>
+            </th>
+            <th>Nazwa</th>
+            <th>Team</th>
+            <th>Tożsamość</th>
+            <th>Wynagrodzenie</th>
+            <th>Siła</th>
+          </tr>
+        </thead>
+        <tbody>
+          {heroes.map((hero) => {
+            return (
+              <tr key={hero.id}>
+                <td>
+                  {showImage && (
+                    <img className="image" src={hero.logoUrl} alt={hero.name} />
+                  )}
+                </td>
+                <td>{hero.name}</td>
+                <td>{hero.team}</td>
+                <td>{hero.secretIdentity}</td>
+                <td>{hero.salary} PLN</td>
+                <td>{hero.strength}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 export default HeroesTable;
